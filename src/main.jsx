c2bd5deb2a0c2917b69fb8d6b26f5ea57b0d5b16
ronidomi <roni.domi001@umb.edu>
2025-08-12 02:25:27 -0400

    vercel deployment test

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
+import { image } from "./assets/images/index_bg.png"
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
         <App />
-        <img src={"src/assets/images/index_bg.png"} alt="Fox laying down" className="img-index"/>
+        <img src={image} alt="Fox laying down" className="img-index"/>
     </React.StrictMode>
 )
